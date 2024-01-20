ea9048a70d1e92e9538a67b8ac77c69bf5580e2d
Jerry <39301414+JK211@users.noreply.github.com>
2024-01-20 17:42:44 +0800

    Update Batch_Signatures_3actions.py

--- Batch_Signatures_3actions.py
+++ Batch_Signatures_3actions.py
@@ -6,7 +6,7 @@
 @Date: 2023-11-27 11:59
 @LastEditor: JK211
 LastEditTime: 2024-1-20
-@Discription:  我们基于GYMe model batch requests using a sequ编写了批量识别的环境，其中动作包括II,BSI,MRI
+@Discription:  我们基于GYM编写了批量识别的环境，其中动作包括II,BSI,MRI
               We wrote the environment for batch identification based on GYM, where the actions include II,BSI,MRI
 @Environment: python 3.7
 '''
